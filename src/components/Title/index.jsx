fadc270e919d281f22ceffbe4b41b357b2f7d2c3
Dadots <bacatedariuspaul@gmail.com>
2025-03-30 22:22:23 +0800

    push changes
    reponsive text title

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -5,11 +5,13 @@ const Title = ({ title, className = "" }) => {
 
   return (
     <h1
-      className={`text-4xl tracking-wide text-lime-900/80 text-center flex flex-col drop-shadow-md ${className}`}
+      className={`text-4xl sm:text-3xl md:text-4xl lg:text-5xl tracking-wide text-lime-900/80 text-center flex flex-col drop-shadow-md ${className}`}
       style={{ fontFamily: "Coneria" }}
     >
       <span>{firstLine}</span>
-      <span className="block text-3xl mt-1">{secondLine}</span>
+      <span className="block text-3xl sm:text-2xl md:text-3xl mt-1">
+        {secondLine}
+      </span>
     </h1>
   );
 };
