1550ea87ff4fca35919dc2dc14b34b1e93d1a278
Dadots <bacatedariuspaul@gmail.com>
2025-03-30 22:26:52 +0800

    push changes
    responsive text

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -5,11 +5,11 @@ const Title = ({ title, className = "" }) => {
 
   return (
     <h1
-      className={`text-4xl sm:text-3xl md:text-4xl lg:text-5xl tracking-wide text-lime-900/80 text-center flex flex-col drop-shadow-md ${className}`}
+      className={`text-2xl sm:text-xl md:text-2xl lg:text-4xl tracking-wide text-lime-900/80 text-center flex flex-col drop-shadow-md ${className}`}
       style={{ fontFamily: "Coneria" }}
     >
       <span>{firstLine}</span>
-      <span className="block text-3xl sm:text-2xl md:text-3xl mt-1">
+      <span className="block text-xl sm:text-sm md:text-3xl lg:text-4xl mt-1">
         {secondLine}
       </span>
     </h1>
