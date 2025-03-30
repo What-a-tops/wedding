106f05bfaceca63f5d54118eba8e2dbaf8008296
Dadots <bacatedariuspaul@gmail.com>
2025-03-30 22:23:03 +0800

    Updates

--- assets/index-CDUjR5ZF.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-Bwx8AG4m.js";const r=({title:t,className:s=""})=>{const[n,l]=t.split(`
-`);return e.jsxs("h1",{className:`text-4xl tracking-wide text-lime-900/80 text-center flex flex-col drop-shadow-md ${s}`,style:{fontFamily:"Coneria"},children:[e.jsx("span",{children:n}),e.jsx("span",{className:"block text-3xl mt-1",children:l})]})};export{r as T};
